a8fb03c79737f1c1506297df78f89ed1f9e6ef8f
TheBlackCat-777 <raul.rivera36@upr.edu>
2025-11-12 22:58:19 +0000

    Implementación de BFT y pruebas con doctest

--- main.cpp
+++ main.cpp
@@ -18,6 +18,15 @@ TEST_CASE("BinarySearchTree basic operations") {
   B.insert(56);
   CHECK(B.toInorderStr() == "1,2,4,10,15,31,56");
   CHECK(B.BFT() == "[[10],[4,15],[2,31],[1,56]]");
+
+  //remove check
+  B.remove(15);
+  CHECK(B.toInorderStr() == "1,2,4,10,31,56");
+
+  //contains check
+  CHECK(B.contains(15) == false);
+
+
   BinarySearchTree<int> C;
   for (auto e :
        {16, 8, 24, 28, 20, 12, 4, 2, 6, 10, 14, 18, 22, 26, 30, 15, 7, 3}) {
@@ -28,3 +37,5 @@ TEST_CASE("BinarySearchTree basic operations") {
   CHECK(C.BFT() ==
         "[[16],[8,24],[4,12,20,28],[2,6,10,14,18,22,26,30],[3,7,15]]");
 }
+
+
